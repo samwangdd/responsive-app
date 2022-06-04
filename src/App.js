27f6f7e26f9f3bc10f711249f2bbc2dbe3c242b5
Sam <wangyysammore@gmail.com>
2022-06-04 17:28:21 +0800

    fix: complete the responsive layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
-import './demo.scss';
+import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +15,38 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className='container'>
+        <div className='card cardLG flex-row flex-jc-sb'>
+         <div className='avatarLG'></div>
+          <div className='info'>
+              <p className='name'>Sam</p>
+              <p className='description'>Developer of FullStack</p>
+            </div>
+        </div>
+        <div className='cardList'>
+          <div className='card cardSM'>
+            <div className='avatarSM'></div>
+            <div className='info'>
+              <p className='name'>Sam</p>
+              <p className='description'>Developer of FullStack</p>
+            </div>
+          </div>
+          <div className='card cardSM'>
+            <div className='avatarSM'></div>
+            <div className='info'>
+              <p className='name'>Sam</p>
+              <p className='description'>Developer of FullStack</p>
+            </div>
+          </div>
+          <div className='card cardSM'>
+            <div className='avatarSM'></div>
+            <div className='info'>
+              <p className='name'>Sam</p>
+              <p className='description'>Developer of FullStack</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
